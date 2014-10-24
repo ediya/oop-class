61af73adccc9f1d080be9d3821f24ab22d829653
ediya <euan-_-V@hanmail.net>
2014-10-24 11:56:23 +0900

    second commit

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -8,6 +8,16 @@ public class MyTest {
 
     public static void main(String[] args){
 
+        int result = new Calculator().getSum(1,2);
+
         Assert.assertThat(1+2, is(3));
+    }
+
+}
+
+class Calculator {
+
+    public int getSum(int n1, int n2){
+        return n1 + n2;
     }
 }
