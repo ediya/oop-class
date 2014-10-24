06eb3f7ec13130907742604b925e7ef8ecab921e
ediya <euan-_-V@hanmail.net>
2014-10-24 12:32:00 +0900

    second commit

---
+++ src/test/java/multi.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by ce416-081 on 2014. 10. 24..
+ */
+public class multi {
+    @Test
+    public void testmulti() throws Exception {
+
+            assertThat(3*2, is(6));
+        }
+
+    }
+
